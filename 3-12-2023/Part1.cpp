574b00530e49161205e60670aa7b7456ba80a491
Adrian101-hnd <69586718+Adrian101-hnd@users.noreply.github.com>
2023-12-05 13:32:36 -0600

    Day 3

--- 3-12-2023/Part1.cpp
+++ 3-12-2023/Part1.cpp
@@ -11,8 +11,39 @@ bool check_num(string num){
     return true;
 }
 
+void delete_numbers(map<int,int>& numbers,int position){
+    int number_size = to_string(numbers[position]).length();
+    numbers.erase(position);
+    //Delete adjacent number
+    if(number_size == 2){
+        if(numbers.find(position - 1) == numbers.end()){
+            numbers.erase(position + 1);
+        }
+        else{
+           numbers.erase(position - 1); 
+        }
+    }
+    else if(number_size == 3){
+        if(numbers.find(position - 1) != numbers.end() && numbers.find(position - 2) != numbers.end()){
+
+            numbers.erase(position - 1);
+            numbers.erase(position - 2);
+        }
+        else if(numbers.find(position + 1) != numbers.end() && numbers.find(position + 2) != numbers.end()){
+            numbers.erase(position + 1);
+            numbers.erase(position + 2);
+        }
+        else{
+            numbers.erase(position + 1);
+            numbers.erase(position - 1);
+        }
+    }
+
+}
+
 
 int main(){
+    int sum = 0;
     ifstream Myfile("input.txt");
     string line1;
     bool y = false;
@@ -32,22 +63,114 @@ int main(){
             if(a){
                 for(int i = 0;i!=line2.size();i++){
                     numbers[x + i] = stoi(line2);
-                    cout<<"Number saved at: "<< x+i <<"\n";
+                    //cout<<"Number saved at: "<< x+i <<"\n";
                 }
                 x += line2.size();
 
             }
             else{
-              symbols[x] = line2;
-              cout<<"Symbol saved at: "<< x <<"\n";
-              x += 1;
+                //cout<<line2<<"\n";
+                if(line2.size() > 1){
+                    string possible_num = "";
+                    for(int i = 0;i!=line2.size();i++){
+                        if(isdigit(line2[i])){
+                            possible_num += line2[i];
+                        }
+                        else{
+                            if(possible_num.size() != 0){
+                                for(int j = 0;j!=possible_num.size();j++){
+                                numbers[x + j] = stoi(possible_num);
+                                //cout<<"Number saved at: "<< x+i <<"\n";
+                            }
+                                x += possible_num.size();
+                                symbols[x] = line2[i];
+                                //cout<<"Symbol saved at: "<< x <<"\n";
+                                x += 1;
+                                possible_num = "";
+                            }
+                            else{
+                                possible_num = "";
+                                symbols[x] = line2[i];
+                                //cout<<"Symbol saved at: "<< x <<"\n";
+                                x += 1;
+                            }
+                        }
+                    }
+                    if(possible_num != ""){
+                                for(int j = 0;j!=possible_num.size();j++){
+                                numbers[x + j] = stoi(possible_num);
+                                //cout<<"Number saved at: "<< x+i <<"\n";
+                            }
+                                x += possible_num.size();
+                    }
+                }
+                else{
+                    symbols[x] = line2;
+                    //cout<<"Symbol saved at: "<< x <<"\n";
+                    x += 1;
+                }
+              
             }
             }
         }
-        //HACER COMPARACION AQUI HORIZONTAL
 
+        //HACER COMPARACION AQUI HORIZONTAL
+        
+        for (auto it = symbols.begin(); it != symbols.end(); ++it) {
+        int position = it->first;
+        if(numbers.find(position-1) != numbers.end()){
+            cout<<"Se agrego: "<<numbers[position-1]<<"Por estar al lado de: " + it->second<<"\n";
+            sum += numbers[position-1];
+            delete_numbers(numbers,position);
+        }
+        if(numbers.find(position+1) != numbers.end()){
+            cout<<"Se agrego: "<<numbers[position+1]<<"Por estar al lado de: " + it->second<<"\n";
+            sum += numbers[position+1];
+            delete_numbers(numbers,position);
+        }
+
+        //std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
+        }   
         if(y){
-            //HACER COMPARACION AQUI VERTICAL Y DIAGONAL
+            for (auto it = symbols.begin(); it != symbols.end(); ++it) {
+
+                //MATCH BOTTOM UP
+                int position = it->first;
+                if(numbers_prev.find(position) != numbers_prev.end()){
+                    cout<<"Se agrego: "<<numbers_prev[position]<<"Por estar al lado de: " + it->second<<"\n";
+                    sum += numbers_prev[position];
+                    delete_numbers(numbers_prev,position);
+                }
+                if(numbers_prev.find(position -1) != numbers_prev.end()){
+                    cout<<"Se agrego: "<<numbers_prev[position-1]<<"Por estar al lado de: " + it->second<<"\n";
+                    sum += numbers_prev[position-1];
+                    delete_numbers(numbers_prev,position);
+                }
+                if(numbers_prev.find(position + 1) != numbers_prev.end()){
+                    cout<<"Se agrego: "<<numbers_prev[position + 1]<<"Por estar al lado de: " + it->second<<"\n";
+                    sum += numbers_prev[position+1];
+                    delete_numbers(numbers_prev,position);
+                }
+            }
+
+            //MATCH TOP DOWN
+            for (auto it = symbols_prev.begin(); it != symbols_prev.end(); ++it) {
+
+                //MATCH BOTTOM UP
+                int position = it->first;
+                if(numbers.find(position) != numbers.end()){
+                    sum += numbers[position];
+                    delete_numbers(numbers,position);
+                }
+                if(numbers.find(position -1) != numbers.end()){
+                    sum += numbers[position-1];
+                    delete_numbers(numbers,position);
+                }
+                if(numbers.find(position + 1) != numbers.end()){
+                    sum += numbers[position+1];
+                    delete_numbers(numbers,position);
+                }
+            }
         }
         else{
             y =!y;
@@ -58,4 +181,5 @@ int main(){
         symbols.clear();
         
     }
+    cout<<"Sum: "<<sum<<"\n";
 }
